41d7dc873cfc690f865843a703b81db4b7d4c79a
Odair Panizzi Júnior <101676143+OdairPanizziJunior@users.noreply.github.com>
2023-07-01 19:39:02 +0200

    Site Pessoal

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+function brazil(bra){
+    bra.src = "../imagens/Flag_of_the_United_Kingdom.png";
+}
+
+
+// FACEBOOK
+function trocarFace(face) {
+    face.src = "../images/footer/icons8-facebook-24-cl.png";
+  }
+  
+  function restaurarFace(face) {
+    face.src = "../images/footer/icons8-facebook-24-bk.png";
+  }
