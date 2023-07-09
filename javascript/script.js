17fce90408cf9e6bc2cf06596baa163ec701a449
Odair Panizzi Júnior <101676143+OdairPanizziJunior@users.noreply.github.com>
2023-07-09 16:49:16 +0200

    Alteração de Imagem Usando JS
    agora é possível alterar a imagem da classe container-jobs.
    
    Implementação da função changeImage

--- javascript/script.js
+++ javascript/script.js
@@ -1,31 +1,4 @@
-function trocaLinkedin(linkedin){
-    linkedin.src = "../imagens/footer/icons8-linkedin-24-cl.png";
-}
-
-function restauraLinkedin(linkedin){
-    linkedin.src = "../imagens/footer/icons8-linkedin-24-bk.png";
-}
-
-function trocaYoutube(youtube){
-    youtube.src = "../imagens/footer/icons8-youtube-24-cl.png";
-}
-
-function restauraYoutube(youtube){
-    youtube.src = "../imagens/footer/icons8-youtube-24-bk.png";
-}
-
-function trocaFacebook(facebook){
-    facebook.src = "../imagens/footer/icons8-facebook-24-cl.png";
-}
-
-function restauraFacebook(facebook){
-    facebook.src = "../imagens/footer/icons8-facebook-24-bk.png";
-}
-
-function trocaInstagram(instagram){
-    instagram.src = "../imagens/footer/icons8-instagram-24-cl.png";
-}
-
-function restauraInstagram(instagram){
-    instagram.src = "../imagens/footer/icons8-instagram-24-bk.png";
-}
+function changeImage(clickedImage) {
+    var rightImage = document.getElementById("imagem-direita");
+    rightImage.src = clickedImage.src;
+  }
